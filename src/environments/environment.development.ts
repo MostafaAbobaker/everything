cc35e9a59f8c2b43eedfa9abab4c27d61b5f429b
MostafaAbobaker <35193789+MostafaAbobaker@users.noreply.github.com>
2025-08-14 15:53:20 +0300

    edit environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: false,
   apiUrl: 'http://everything.runasp.net/',
-  //apiUrl: 'https://localhost:7162/',
   imagePath: 'https://everyback.bsite.net/',
 
 };
