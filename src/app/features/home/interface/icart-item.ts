122abc32a07796e371ae9e1f0329d392c40a4dc5
HassanAmer11 <hassan.amer.2554@gmail.com>
2025-09-06 11:29:31 +0300

    Hassan-6-9-25

--- src/app/features/home/interface/icart-item.ts
+++ src/app/features/home/interface/icart-item.ts
@@ -1,17 +1,26 @@
 export interface IcartItem {
-    "cartId": number,
-    "quantity": number,
-    "unitPrice": number,
-    "totalPrice": number,
-    "productId": string,
-    "productName": string,
-    "userId": string,
-    "featuresId": number,
-    "addedDate": string,
-    "productImage":string
+  cartId: number;
+  quantity: number;
+  unitPrice: number;
+  totalPrice: number;
+  productId: string;
+  productName: string;
+  userId: string;
+  featuresId: number;
+  addedDate: string;
+  productImage: string;
+  productsId: string;
 }
 
 export interface PaymentOrder {
-    "orderPaymentMethod": number,
-    "userId": string,
+  orderPaymentMethod: number;
+  userId: string;
+}
+
+export interface Order {
+  orderDate: string;
+  orderId: number;
+  orderItems: IcartItem[];
+  totalPrice: number;
+  orderStatus: number;
 }
